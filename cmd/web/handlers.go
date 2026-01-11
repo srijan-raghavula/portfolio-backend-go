850a68d9b66f3c7567cc4d3ded684185f502db9e
srijan-raghavula <asktelugu@gmail.com>
2026-01-11 18:44:37 +0530

    add resource endpoints, test endpoints to be removed

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,3 +9,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		Message: "sweet home alabama",
 	}, w, http.StatusOK)
 }
+
+func (app *application) tbd(w http.ResponseWriter, r *http.Request) {
+	app.sendMessage(&Payload{
+		Message: "You ain't dun doin' dis yet.",
+	}, w, http.StatusOK)
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,25 @@ func (app *application) routes() http.Handler {
 
 	mux.HandleFunc("GET /{$}", app.home)
 
+	// The following endpoints are separated by category
+	mux.HandleFunc("POST /v0/sessions", app.tbd)
+	mux.HandleFunc("DELETE /v0/sessions", app.tbd)
+
+	mux.HandleFunc("GET /v0/profile", app.tbd)
+	mux.HandleFunc("PUT /v0/profile", app.tbd)
+
+	mux.HandleFunc("GET /v0/blogs", app.tbd)
+	mux.HandleFunc("GET /v0/blogs/{id}", app.tbd)
+	mux.HandleFunc("POST /v0/blogs", app.tbd)
+	mux.HandleFunc("PUT /v0/blogs/{id}", app.tbd)
+	mux.HandleFunc("DELETE /v0/blogs/{id}", app.tbd)
+
+	mux.HandleFunc("GET /v0/projects", app.tbd)
+	mux.HandleFunc("GET /v0/projects/{id}", app.tbd)
+	mux.HandleFunc("POST /v0/projects", app.tbd)
+	mux.HandleFunc("PUT /v0/projects/{id}", app.tbd)
+	mux.HandleFunc("DELETE /v0/projects/{id}", app.tbd)
+
 	std := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return std.Then(mux)
 }
